89164b36f23fd5886621b53fd2afcd123cfc859a
Pawel Marczewski <humpolec@gmail.com>
2012-06-17 17:56:37 +0200

    quasiquotes handled correctly by macroexpansion

--- lisp-macro.js
+++ lisp-macro.js
@@ -3,19 +3,22 @@
 lisp.macros = {};
 
 // Perform one step of macroexpansion for given source term
-// (source code). If hasHead is false, do not append to invoke
-// a macro in the head of the term.
-lisp.macroExpandOne = function(term, hasHead) {
+// (source code).
+// hasHead - if false, do not append to invoke a macro in the
+//   head of the term
+// quasiLevel - if >0, we're inside a quasiquote
+lisp.macroExpandOne = function(term, hasHead, quasiLevel) {
     if (term.type != 'cons')
         return null;
 
     // first, try expanding the head
-    var car = lisp.macroExpandOne(term.car, true);
+    var car = lisp.macroExpandOne(term.car, true, quasiLevel);
     if (car != null)
         return new lisp.Cons(car, term.cdr);
 
     // now, maybe the term itself is a macro invocation
     if (hasHead &&
+        quasiLevel == 0 &&
         term.car.type == 'symbol' &&
         term.car.s in lisp.macros) {
 
@@ -24,13 +27,21 @@ lisp.macroExpandOne = function(term, hasHead) {
         return func.run(args);
     }
 
-    if (term.car.type == 'symbol' &&
+    if (quasiLevel == 0 &&
+        term.car.type == 'symbol' &&
         term.car.s == 'quote')
         // we don't expand quoted terms
         return null;
 
+    if (hasHead && term.car.type == 'symbol') {
+        if (term.car.s == 'quasiquote')
+            quasiLevel++;
+        if (term.car.s == 'unquote')
+            quasiLevel--;
+    }
+
     // lastly, try expanding the rest of list
-    var cdr = lisp.macroExpandOne(term.cdr, false);
+    var cdr = lisp.macroExpandOne(term.cdr, false, quasiLevel);
     if (cdr != null)
         return new lisp.Cons(term.car, cdr);
 
@@ -39,7 +50,7 @@ lisp.macroExpandOne = function(term, hasHead) {
 
 lisp.macroExpand = function(term) {
     var newTerm;
-    while ((newTerm = lisp.macroExpandOne(term, true)) != null)
+    while ((newTerm = lisp.macroExpandOne(term, true, 0)) != null)
         term = newTerm;
     return term;
 };
